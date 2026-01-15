19b12f6d2a00d13bfeb6ef78b3a0ee35c5c2d406
colly-j <jdasol0407@gmail.com>
2026-01-15 13:29:03 +0900

    전체 수정

--- js/fonts.js
+++ js/fonts.js
@@ -1,29 +1,19 @@
-// Adobe Fonts (Typekit) 로딩
-(function (d) {
+// Adobe Fonts 프로젝트 1 - totalblack-variable (로고용)
+(function(d) {
   var config = {
-      kitId: "wqo4akj",
-      scriptTimeout: 3000,
-      async: true,
-    },
-    h = d.documentElement,
-    t = setTimeout(function () {
-      h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
-    }, config.scriptTimeout),
-    tk = d.createElement("script"),
-    f = false,
-    s = d.getElementsByTagName("script")[0],
-    a;
-  h.className += " wf-loading";
-  tk.src = "https://use.typekit.net/" + config.kitId + ".js";
-  tk.async = true;
-  tk.onload = tk.onreadystatechange = function () {
-    a = this.readyState;
-    if (f || (a && a != "complete" && a != "loaded")) return;
-    f = true;
-    clearTimeout(t);
-    try {
-      Typekit.load(config);
-    } catch (e) {}
-  };
-  s.parentNode.insertBefore(tk, s);
+    kitId: 'pyk6bts',
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
+
+// Adobe Fonts 프로젝트 2 - futura-pt 등 (본문용)
+(function(d) {
+  var config = {
+    kitId: 'wqo4akj',
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
